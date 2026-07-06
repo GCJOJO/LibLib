db9a0a19fd4dc8285670129f83faabb52be82e83
GCJOJO <39382270+GCJOJO@users.noreply.github.com>
2026-07-06 22:27:48 +0200

    APIs to let QuestsLib and BlablaLib communicate

--- common/src/main/java/io/github/gcjojo/liblib/api/BlablaLibAPI.java
+++ common/src/main/java/io/github/gcjojo/liblib/api/BlablaLibAPI.java
@@ -3,26 +3,50 @@ package io.github.gcjojo.liblib.api;
 import dev.architectury.event.Event;
 import dev.architectury.event.EventFactory;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+
+import java.util.Optional;
 
 public abstract class BlablaLibAPI {
     public static Event<DialogueCompleted> DIALOGUE_COMPLETED = EventFactory.createLoop();
 
-    public abstract void openDialogue(ResourceLocation dialogueId);
+    public abstract void openDialogue(ServerPlayer player, ResourceLocation dialogueId);
 
-    public abstract void setCurrentDialogue(ResourceLocation dialogueId);
+    public abstract void setCurrentDialogue(ServerPlayer player, ResourceLocation dialogueId);
+
+    public abstract boolean isPlayerInDialogue(ServerPlayer player);
+
+    public abstract Optional<ResourceLocation> getPlayerLastReadDialogue(ServerPlayer player);
+
+    public abstract void setPlayerLastReadDialogue(ServerPlayer player, ResourceLocation lastReadDialogueId);
 
     public interface DialogueCompleted {
-        public void onDialogueCompleted(ResourceLocation dialogueId);
+        public void onDialogueCompleted(ServerPlayer player, ResourceLocation dialogueId);
     }
 
     public static class EmptyBlablaLibAPI extends BlablaLibAPI {
 
         @Override
-        public void openDialogue(ResourceLocation dialogueId) {
+        public void openDialogue(ServerPlayer player, ResourceLocation dialogueId) {
         }
 
         @Override
-        public void setCurrentDialogue(ResourceLocation dialogueId) {
+        public void setCurrentDialogue(ServerPlayer player, ResourceLocation dialogueId) {
+        }
+
+        @Override
+        public boolean isPlayerInDialogue(ServerPlayer player) {
+            return false;
+        }
+
+        @Override
+        public Optional<ResourceLocation> getPlayerLastReadDialogue(ServerPlayer player) {
+            return Optional.empty();
+        }
+
+        @Override
+        public void setPlayerLastReadDialogue(ServerPlayer player, ResourceLocation lastReadDialogueId) {
+
         }
     }
 }
--- common/src/main/java/io/github/gcjojo/liblib/api/QuestsLibAPI.java
+++ common/src/main/java/io/github/gcjojo/liblib/api/QuestsLibAPI.java
@@ -3,25 +3,26 @@ package io.github.gcjojo.liblib.api;
 import dev.architectury.event.Event;
 import dev.architectury.event.EventFactory;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
 
 public abstract class QuestsLibAPI {
     public static Event<QuestCompleted> QUEST_COMPLETED = EventFactory.createLoop();
     public static Event<TaskCompleted> TASK_COMPLETED = EventFactory.createLoop();
 
-    public abstract void startQuest(ResourceLocation questId);
+    public abstract void startQuest(Player player, ResourceLocation questId);
 
     public interface QuestCompleted {
-        public void onQuestCompleted(ResourceLocation questId);
+        public void onQuestCompleted(Player player, ResourceLocation questId);
     }
 
     public interface TaskCompleted {
-        public void onTaskCompleted(ResourceLocation taskId);
+        public void onTaskCompleted(Player player, ResourceLocation taskId);
     }
 
     public static class EmptyQuestsLibAPI extends QuestsLibAPI {
 
         @Override
-        public void startQuest(ResourceLocation questId) {
+        public void startQuest(Player player, ResourceLocation questId) {
         }
     }
 }
